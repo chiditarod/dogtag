a08f0f398e88bb307fd979c8793ccdd86ec3011f
Devin Breen <dbreen@groupon.com>
2013-02-28 02:12:48 -0600

    Inital Commit: Ruby 2.0 and Rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsSkeleton::Application.config.secret_key_base = 'cc38079f9266743e9d3a4160751838541f306e3ce61401e89b9e1997ebbd042025a27cca0fa2cc4ceb38493ad9ae99a703e1a67bcd4f6cfc2f62754927e11abb'
