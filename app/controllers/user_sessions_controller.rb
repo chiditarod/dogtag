3726bcf58d726159dbfff4aec0938ee6804d4f19
Devin Breen <dbreen@groupon.com>
2013-12-27 16:44:50 -0600

    fixed a randomly erroring authlogic spec

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -10,17 +10,17 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.new(params[:user_session])
     logger.info params[:user_session]
     if @user_session.save
-      flash[:notice] = t '.login_success'
+      flash[:notice] = t('.login_success')
       redirect_back_or_default account_url
     else
-      flash[:error] = t '.login_failed'
+      flash[:error] = t('.login_failed')
       render :action => :new
     end
   end
 
   def destroy
     current_user_session.destroy
-    flash[:notice] = t '.logout_success'
+    flash[:notice] = t('.logout_success')
     redirect_back_or_default new_user_session_url
   end
 end
--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -5,6 +5,10 @@ describe UserSessionsController do
   let(:user_session) { FactoryGirl.create :user_session }
   let(:user_session_hash) { FactoryGirl.attributes_for :user_session }
 
+  before do
+    activate_authlogic
+  end
+
   describe '#new' do
     it 'returns http success and calls UserSession.new' do
       session_stub = UserSession.new
@@ -14,26 +18,27 @@ describe UserSessionsController do
     end
   end
 
-  describe '#create' do
-    it 'saves a new user session and sets flash notice'# do
+  #todo - get these working or get rid of them.
+  #describe '#create' do
+    #it 'saves a new user session and sets flash notice' do
       #expect do
         #post :create, :user_session => user_session_hash
         #response.status.should == 302
-        #flash[:notice].should == 'Logout in successful.'
+        #flash[:notice].should == 'Login successful.'
       #end.to change(UserSession, :count).by 1
     #end
 
-    it 'sets flash and renders new if user session cannot be saved'# do
+    #it 'sets flash and renders new if user session cannot be saved' do
       #UserSession.should_receive(:new).and_return mock_session
       #mock_session.should_receive(:save).and_return false
       #post :create, :user_session => user_session_hash
       #flash[:error].should == 'Login failed.'
       #response.should redirect_to new_user_session
     #end
-  end
+  #end
 
-  describe '#destroy' do
-    it 'destroys the current user session, sets flash, and redirects'
-  end
+  #describe '#destroy' do
+    #it 'destroys the current user session, sets flash, and redirects'
+  #end
 
 end
