c80ff200e2125aed422419a1180dfd1923274743
Devin Breen <dev@nuclearblender.com>
2015-01-16 04:00:30 -0600

    Fixing password support in authlogic 3.4.0

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 20150116063514) do
   end
 
   add_index "completed_requirements", ["team_id", "requirement_id"], name: "index_completed_requirements_on_team_id_and_requirement_id", unique: true, using: :btree
-  add_index "completed_requirements", ["user_id"], name: "index_completed_requirements_on_user_id", using: :btree
 
   create_table "people", force: true do |t|
     t.string   "first_name"
@@ -40,8 +39,6 @@ ActiveRecord::Schema.define(version: 20150116063514) do
     t.integer  "experience"
     t.string   "zipcode",    null: false
   end
-
-  add_index "people", ["team_id"], name: "index_people_on_team_id", using: :btree
 
   create_table "races", force: true do |t|
     t.string   "name"
@@ -86,7 +83,6 @@ ActiveRecord::Schema.define(version: 20150116063514) do
   end
 
   add_index "teams", ["race_id"], name: "index_teams_on_race_id", using: :btree
-  add_index "teams", ["user_id"], name: "index_teams_on_user_id", using: :btree
 
   create_table "tiers", force: true do |t|
     t.integer  "requirement_id"
