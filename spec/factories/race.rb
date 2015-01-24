5b32bd37c11987fe8517ad079cad364c5692cde8
Devin Breen <dev@nuclearblender.com>
2015-01-24 13:52:23 -0600

    Ensured proper flow when a race has no jsonform data set Started specs for questions_controller

--- spec/factories/race.rb
+++ spec/factories/race.rb
@@ -12,7 +12,8 @@ FactoryGirl.define do
       registration_close (Time.now - 1.week)
     end
 
-    #trait :with_jsonform do
-    #end
+    trait :with_jsonform do
+      jsonform File.read(Rails.root.to_s + '/spec/fixtures/files/valid_jsonform.json')
+    end
   end
 end
