7a8d2c88cd621beeabf40cb0d2b3fb8cf7c10512
Devin Breen <dbreen@groupon.com>
2017-05-31 10:20:16 -0500

    Added GPLv3 header to each source code file

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,18 @@
+# Copyright (C) 2013 Devin Breen
+# This file is part of dogtag <https://github.com/chiditarod/dogtag>.
+#
+# dogtag is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# dogtag is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with dogtag.  If not, see <http://www.gnu.org/licenses/>.
 class Person < ActiveRecord::Base
   validates_presence_of :first_name, :last_name, :email, :phone, :zipcode
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
