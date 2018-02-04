7e3a35337fd7118a4252b16746e13325a538a56a
Devin Breen <dev@nuclearblender.com>
2018-02-03 22:09:12 -0600

    Tell Rollbar to ignore ActionController::RoutingError exceptions

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -11,6 +11,10 @@ Rollbar.configure do |config|
     config.enabled = false
   end
 
+
+
+
+
   # By default, Rollbar will try to call the `current_user` controller method
   # to fetch the logged-in user object, and then call that object's `id`,
   # `username`, and `email` methods to fetch those properties. To customize:
@@ -33,6 +37,8 @@ Rollbar.configure do |config|
   #
   # You can also specify a callable, which will be called with the exception instance.
   # config.exception_level_filters.merge!('MyCriticalException' => lambda { |e| 'critical' })
+
+  config.exception_level_filters.merge!('ActionController::RoutingError' => 'ignore')
 
   # Enable asynchronous reporting (uses girl_friday or Threading if girl_friday
   # is not installed)
