e29f76774a619335cd24875d4e9146e98ee51014
Devin Breen <dev@nuclearblender.com>
2022-01-19 07:30:02 -0600

    Configure redis per heroku instructions

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -16,12 +16,12 @@
 redis_url = ENV['REDIS_URL'] || "redis://127.0.0.1:6379/0"
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: redis_url }
+  config.redis = { url: redis_url, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
   config.failures_max_count = false # no limit on number of failing jobs in flight
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: redis_url }
+  config.redis = { url: redis_url, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
 end
 
 # password-protect the /sidekiq route
