dd9faaf80e07581fe1a1d350097d147961b9fb57
Devin Breen <dev@nuclearblender.com>
2022-11-29 17:06:05 -0600

    Remove reference to objects removed after rails 5.1.7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,6 @@ class ApplicationController < ActionController::Base
   unless Rails.configuration.consider_all_requests_local
     rescue_from Exception, :with => :render_error
     rescue_from ActionController::RoutingError, :with => :render_not_found
-    rescue_from ActionController::UnknownController, :with => :render_not_found
     rescue_from CanCan::AccessDenied, :with => :render_access_denied
   end
   # we always want these (including during tests)
