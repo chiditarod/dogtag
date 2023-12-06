94691b5a55a5a965651d06cd7f82afa3da3c526b
Devin Breen <dev@nuclearblender.com>
2023-12-05 22:13:17 -0600

    force ssl (https://edgeguides.rubyonrails.org/upgrading_ruby_on_rails.html\#force-ssl)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,9 +18,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # SSL
-  force_ssl :if => :is_production?
-
   helper :all
   helper_method :current_user
 
@@ -47,10 +44,6 @@ class ApplicationController < ActionController::Base
   end
 
   private
-
-  def is_production?
-    Rails.env.production?
-  end
 
   def render_not_found(ex)
     log_error(ex)
